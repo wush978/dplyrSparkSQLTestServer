53ac997c04e4cf00c15ea8544cb2b43a7b19ebd8
Wush Wu <wush978@gmail.com>
2015-05-13 02:10:43 +0800

    use future to run thrift server asynchronized

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,8 @@
+import java.sql.{SQLException, DriverManager}
+
+import scala.annotation.tailrec
+import scala.concurrent._
+import ExecutionContext.Implicits.global
 import org.apache.spark.deploy.SparkSubmit
 
 /**
@@ -5,14 +10,33 @@ import org.apache.spark.deploy.SparkSubmit
  */
 object Main {
 
+  val driverName = "org.apache.hadoop.hive.jdbc.HiveDriver";
+
+  Class.forName(driverName)
+
+  @tailrec
+  def checkConnection() : Unit = {
+    try {
+      println("Test Connection...")
+      val con = DriverManager.getConnection("jdbc:hive2://localhost:10000", "", "")
+    }
+    catch {
+      case e : SQLException => {
+        Thread.sleep(5000)
+        checkConnection()
+      }
+    }
+  }
+
   def main(args : Array[String]) : Unit = {
     val default_args : Array[String] = """
                                          |--class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2
                                          |spark-internal
                                        """.stripMargin.replace('\n', ' ').split(" ").filter(_.size > 0)
-
-    SparkSubmit.main(default_args ++ args)
-
+    future {
+      SparkSubmit.main(default_args ++ args)
+    }
+    checkConnection()
+    println("Done!")
   }
-
 }
